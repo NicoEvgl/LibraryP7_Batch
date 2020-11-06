46cb7107475bac75917f6b4e038946b85a2cfba3
Nico Evgl <nico.evangelista33@gmail.com>
2020-11-06 15:10:12 +0100

    first commit - Build Maven project / pom.xml

---
+++ src/main/java/com/nicoe/library/LibraryBatch.java
@@ -0,0 +1,13 @@
+package com.nicoe.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBatch {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBatch.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicoe/library/LibraryBatchTests.java
@@ -0,0 +1,13 @@
+package com.nicoe.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBatchTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
