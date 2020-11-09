3739ee96b175c6fdcbf9be3ee82ddd8bf7893da1
Nico Evgl <nico.evangelista33@gmail.com>
2020-11-09 17:36:29 +0100

    fix bugs + refactors

--- src/main/java/com/nicoe/library/proxies/LibraryProxy.java
+++ src/main/java/com/nicoe/library/proxies/LibraryProxy.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name = "LibraryP7_API", url = "localhost:9090")
+@FeignClient(name = "LibraryApi", url = "localhost:9090")
 public interface LibraryProxy {
 
     @GetMapping("/delaysList")
